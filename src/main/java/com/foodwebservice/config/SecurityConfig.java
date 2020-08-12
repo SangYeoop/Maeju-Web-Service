6b5ca062ea8cdc99ec4a5d45131613b7b283d338
SangYeoop <tkdduq8260@gmail.com>
2020-08-12 20:13:44 +0900

    Make Food Recommend Web Service Project

---
+++ src/main/java/com/foodwebservice/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.foodwebservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .mvcMatchers("/", "/login").permitAll();
+    }
+
+}
