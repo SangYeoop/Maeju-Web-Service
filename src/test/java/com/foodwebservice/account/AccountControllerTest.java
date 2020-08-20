af0ee48457a5645f244f94bba6f9d09854574ccb
SangYeoop <tkdduq8260@gmail.com>
2020-08-21 00:02:37 +0900

    Delete unused module

--- src/test/java/com/foodwebservice/account/AccountControllerTest.java
+++ src/test/java/com/foodwebservice/account/AccountControllerTest.java
@@ -1,6 +1,5 @@
 package com.foodwebservice.account;
 
-import com.foodwebservice.account.form.SignUpForm;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
